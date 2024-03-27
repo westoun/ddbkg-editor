7a3a9cc32fcc194c026404e13d92b5ecc65bd3e8
westoun <christoph.werner.stein@gmail.com>
2024-03-27 15:31:31 +0100

    Dockerize the project

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  triplesEndpoint: window.location.origin + '/api/triples',
+  tripleUpdatesEndpoint: window.location.origin + '/api/update',
 };
